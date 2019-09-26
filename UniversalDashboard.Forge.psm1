3eae4b1bcea6319686ad97077cddf47bdf7dd416
Adam Driscoll <adam.r.driscoll@gmail.com>
2019-09-26 03:08:05 -0600

    Fixed some issues. Added help.

--- UniversalDashboard.Forge.psm1
+++ UniversalDashboard.Forge.psm1
@@ -1,40 +1,85 @@
-function New-UDDesktopApp {
+function New-UDDesktopApp {  
+    <#
+    .SYNOPSIS
+    Generate a desktop app with Universal Dashboard
+    
+    .DESCRIPTION
+    Generates a desktop application with Universal Dashboard using electron. 
+    
+    .PARAMETER Path
+    The path to the dashboard.ps1 file you want to create an app out of. 
+    
+    .PARAMETER Name
+    The name of the electron application. 
+    
+    .PARAMETER OutputPath
+    The output path for the application. 
+    
+    .EXAMPLE
+    New-UDDesktopApp -Path "./dashboard.ps1" -OutputPath "./out" -Name "MyApp"
+    
+    .NOTES
+    This cmdlet requires NodeJS to be installed. 
+    #>
+      
     param(
+        [Parameter(Mandatory)]
         $Path, 
+        [Parameter(Mandatory)]
         $Name,
+        [Parameter()]
         $OutputPath
     )
 
-    $Npx = Get-Command npx 
-    if ($null -eq $Npx)
-    {
-        throw "NodeJS is required to run New-UDDesktopApp. Download here: https://nodejs.org"
+    End {
+        $Npx = Get-Command npx 
+        if ($null -eq $Npx)
+        {
+            throw "NodeJS is required to run New-UDDesktopApp. Download here: https://nodejs.org"
+        }
+    
+        if ($null -eq $OutputPath)
+        {
+            $OutputPath = $PSScriptRoot
+            Write-Verbose "No output path specified. Using: $OutputPath"
+        }
+        else
+        {
+            $provider = $null;
+            $drive = $null
+            $pathHelper = $ExecutionContext.SessionState.Path
+            $OutputPath = $pathHelper.GetUnresolvedProviderPathFromPSPath($OutputPath, [ref]$provider, [ref]$drive)
+            Write-Verbose "Output path resolved to: $OutputPath"
+        }
+    
+        if (Test-Path (Join-Path $OutputPath $Name))
+        {
+            Write-Verbose "Output path exists. Removing existing output path."
+            Remove-Item (Join-Path $OutputPath $Name) -Force -Recurse
+        }
+    
+        if (-not (Test-Path $OutputPath))
+        {
+            Write-Verbose "Output path does not exist. Creating new output path."
+            New-Item -Path $OutputPath -ItemType Directory | Out-Null
+        }
+    
+        Push-Location $OutputPath
+        Write-Verbose "Creating electron app $Name"
+        npx create-electron-app $Name
+        Pop-Location
+    
+        $src = [IO.Path]::Combine($OutputPath, $Name, 'src')
+
+        Write-Verbose "Copying dashboard and index.js to electron src folder: $src"
+    
+        Copy-Item -Path $Path -Destination $src
+        Copy-Item -Path (Join-Path $PSScriptRoot "index.js" ) -Destination $src -Force
+
+        Write-Verbose "Building electron app with forge"
+    
+        npm i -g @electron-forge/cli
+        Set-Location (Join-Path $OutputPath $Name)
+        electron-forge make
     }
-
-    if ($null -eq $OutputPath)
-    {
-        $OutputPath = $PSScriptRoot
-    }
-
-    if (Test-Path (Join-Path $OutputPath $Name))
-    {
-        Remove-Item (Join-Path $OutputPath $Name) -Force -Recurse
-    }
-
-    if (-not (Test-Path $OutputPath))
-    {
-        New-Item -Path $OutputPath -ItemType Directory | Out-Null
-    }
-
-    Set-Location $OutputPath
-    npx create-electron-app $Name
-
-    $src = [IO.Path]::Combine($OutputPath, $Name, 'src')
-
-    Copy-Item -Path $Path -Destination $src
-    Copy-Item -Path (Join-Path $PSScriptRoot "index.js" ) -Destination $src -Force
-
-    npm i -g @electron-forge/cli
-    Set-Location (Join-Path $OutputPath $Name)
-    electron-forge make
 }
--- test/Forge.Tests.ps1
+++ test/Forge.Tests.ps1
@@ -1,2 +1,1 @@
-Import-Module (Join-Path $PSScriptRoot "../UniversalDashboard.Forge.psm1") -Force
-New-UDDesktopApp -Name "MyApp" -Path (Join-Path $PSScriptRoot "dashboard.ps1") -OutputPath "$PSScriptRoot/Out"
+New-UDDesktopApp -Name "MyApp" -Path "./dashboard.ps1" -OutputPath "./Out" -Verbose
