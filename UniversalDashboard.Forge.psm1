eacb564d52d94dced7d1b024f5a396931a9f97c2
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2019-10-04 17:14:14 -0700

    get first in array

--- UniversalDashboard.Forge.psm1
+++ UniversalDashboard.Forge.psm1
@@ -142,11 +142,11 @@ Import-Module UniversalDashboard
         $squirrelSplat = @{'ConfigPath' = $packageConfig}
         if ($IconUrl) {$squirrelSplat['IconUrl'] = $IconUrl}
         if ($SetupIcon) {
-            $iconPath = (Get-ChildItem -Path $src -Filter (Split-Path $SetupIcon -Leaf) -Recurse).FullName
+            $iconPath = (Get-ChildItem -Path $src -Filter (Split-Path $SetupIcon -Leaf) -Recurse)[0].FullName
             $squirrelSplat['SetupIcon'] = $iconPath
         }
         if ($LoadingGif) {
-            $gifPath = (Get-ChildItem -Path $src -Filter (Split-Path $LoadingGif -Leaf) -Recurse).FullName
+            $gifPath = (Get-ChildItem -Path $src -Filter (Split-Path $LoadingGif -Leaf) -Recurse)[0].FullName
             $squirrelSplat['LoadingGif'] = $gifPath
         }
         Set-SquirrelConfig @squirrelSplat
